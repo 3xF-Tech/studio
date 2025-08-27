d2b0b862033365924489ba1ea8987679289e7f93
Danilo Moreira <Danillo.moreira.silva@gmail.com>
2025-08-27 20:00:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="w-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
       </header>
 
       <main className="flex-1">
-        <section className="w-full py-12 md:py-16 lg:py-20">
+        <section className="w-full pt-6 pb-12 md:pt-8 md:pb-16 lg:pt-10 lg:pb-20">
           <div className="container px-4 md:px-6">
             <div className="grid gap-8 lg:grid-cols-2 lg:gap-16">
               <div className="flex flex-col justify-center space-y-6">
